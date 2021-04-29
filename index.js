08766af0bba5cb23634c42f62bae9632594f6d1f
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-28 20:54:07 -0400

    commit tree 8547dffcfb604abb360e1c693a3ae6e7d16c8ab3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function ifWin(obj){
+   return obj.result === 'W'
+}
 
+function superbowlWin (array) {
+    const win = array.find(ifWin)
+    if (win != undefined) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
